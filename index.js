ef90ace9d122f93a855544eb41400451e0929f42
Jorge Mantilla <33700227+manto465@users.noreply.github.com>
2023-04-25 16:53:10 -0400

    back end folder structure complete

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+//npm init i
+//nodemon i
+//mysql
+//knex
+//added start:nodedom to package
+//router
+// scripts added from cda
---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'rootroot',
+        database: 'in_stock',
+        charset: 'utf8',
+    },
+};
