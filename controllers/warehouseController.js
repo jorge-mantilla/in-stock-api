8ab5373c7aed2510404a219e46af224bd5fdab57
aborgono <albertoiborgonovo@gmail.com>
2023-04-26 12:55:44 -0400

    added single warehouse to routes and controllers

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -8,3 +8,20 @@ exports.index = (_req, res) => {
     })
     .catch((err) => res.status(400).send(`Error retrieving Warehouses ${err}`));
 };
+
+exports.singleWarehouse = (req, res) => {
+    knex('warehouses')
+    .where({ id: req.params.id })
+    .then((data) => {
+    // If record is not found, respond with 404
+    if (!data.length) {
+        return res.status(404).send(`Record with id: ${req.params.id} is not found`);
+    }
+
+    // Knex returns an array of records, so we need to send response with a single object only
+    res.status(200).json(data[0]);
+    })
+    .catch((err) =>
+    res.status(400).send(`Error retrieving warehouse ${req.params.id} ${err}`)
+    );
+};
--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -3,4 +3,8 @@ const warehouseController = require('../controllers/warehouseController');
 
 router.route('/').get(warehouseController.index);
 
+router.route('/:id').get(warehouseController.singleWarehouse);
+
+
+
 module.exports = router;
