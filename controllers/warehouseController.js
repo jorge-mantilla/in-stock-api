e5de114f8aea934b5dea0233b19288033b5dd0c2
aborgono <albertoiborgonovo@gmail.com>
2023-04-25 23:09:00 -0400

    confirmed routes, controllers, added env file, and restructured knexfile

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -2,10 +2,9 @@ const knex = require('knex')(require('../knexfile'));
 
 exports.index = (_req, res) => {
     knex('warehouse')
+    .select('id', 'name', 'manager')
     .then((data) => {
-        res.status(200).json(data);
+    res.status(200).json(data);
     })
-    .catch((err) =>
-        res.status(400).send(`Error retrieving Warehouses: ${err}`)
-    );
+    .catch((err) => res.status(400).send(`Error retrieving Warehouses ${err}`));
 };
