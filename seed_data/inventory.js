61659d0b093ec72a5859d516e0992118b17ef974
aborgono <albertoiborgonovo@gmail.com>
2023-04-25 22:45:55 -0400

    completed migration and set controllers and routes

--- seed_data/inventory.js
+++ seed_data/inventory.js
@@ -1,28 +1,28 @@
-// // seed data from cda to test axios gets
+// seed data from cda to test axios gets
 
-// module.exports = [
-//     {
-//       id: 1,
-//       name: 'Product One',
-//       description: 'one awesome product for photographers',
-//       quantity: 400,
-//       status: 'In Stock',
-//       warehouse_id: 2,
-//     },
-//     {
-//       id: 2,
-//       name: 'Product Two',
-//       description: 'one awesome product for chefs',
-//       quantity: 800,
-//       status: 'In Stock',
-//       warehouse_id: 3,
-//     },
-//     {
-//       id: 3,
-//       name: 'Product One',
-//       description: 'one awesome product for musicians',
-//       quantity: 90,
-//       status: 'Out Of Stock',
-//       warehouse_id: 1,
-//     },
-//   ];
+module.exports = [
+    {
+      id: 1,
+      name: 'Product One',
+      description: 'one awesome product for photographers',
+      quantity: 400,
+      status: 'In Stock',
+      warehouse_id: 2,
+    },
+    {
+      id: 2,
+      name: 'Product Two',
+      description: 'one awesome product for chefs',
+      quantity: 800,
+      status: 'In Stock',
+      warehouse_id: 3,
+    },
+    {
+      id: 3,
+      name: 'Product One',
+      description: 'one awesome product for musicians',
+      quantity: 90,
+      status: 'Out Of Stock',
+      warehouse_id: 1,
+    },
+  ];
